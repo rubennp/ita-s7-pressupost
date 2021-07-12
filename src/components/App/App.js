fbdbe2264f76ba8f5cbe191771ed67ce3e626057
Rubèn Nieto <ruben.np@me.com>
2021-07-12 09:22:40 +0200

    Fi ex4

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,23 @@ function App() {
     });
   const [total, setTotal] = useState(0);
 
-  const CalcPressupost = () => {
+  const chPags = (pags) => setPressupost({...pressupost, nPags: pags});
+  const chIdiomes = (idiomes) => setPressupost({...pressupost, nIdiomes: idiomes});
+
+  // agafa dades del pressupost de localStorage, si existeixen, només en inicialitzar
+  useEffect(() => {
+    const onStorage = JSON.parse(localStorage.getItem('pressupost'));
+
+    if (!onStorage) {
+      localStorage.setItem('pressupost', JSON.stringify(pressupost));
+    } else {
+      setPressupost(onStorage);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // calcula pressupost: 
     let total = 0;
 
     if (pressupost.web) {
@@ -31,22 +47,20 @@ function App() {
     if (pressupost.ads) total += 200;
 
     setTotal(total);
-  };
 
-  const chPags = (pags) => setPressupost({...pressupost, nPags: pags});
-  const chIdiomes = (idiomes) => setPressupost({...pressupost, nIdiomes: idiomes});
-
-  useEffect(CalcPressupost,[pressupost]);
+    // guarda pressupost a localStorage
+    localStorage.setItem('pressupost', JSON.stringify(pressupost));
+  },[pressupost]);
 
   return (
     <div className="App">
       <GlobalStyle />
       <h1>Que vols fer?</h1>
       <Pressupost>
-        <p><label><input type="checkbox" onChange={(e) => setPressupost({...pressupost, web: e.target.checked, nPags: 1, nIdiomes: 1}) } />Una pàgina web (500€)</label></p>
+        <p><label><input checked={pressupost.web} type="checkbox" onChange={(e) => setPressupost({...pressupost, web: e.target.checked, nPags: 1, nIdiomes: 1}) } />Una pàgina web (500€)</label></p>
         { pressupost.web && <Panell p={pressupost} chPags={chPags} chIdiomes={chIdiomes}/> }
-        <p><label><input type="checkbox" onChange={(e) => setPressupost({...pressupost, seo: e.target.checked}) } />Una consultoría SEO (300€)</label></p>
-        <p><label><input type="checkbox" onChange={(e) => setPressupost({...pressupost, ads: e.target.checked}) } />Una campanya de Google Ads (200€)</label></p>
+        <p><label><input checked={pressupost.seo} type="checkbox" onChange={(e) => setPressupost({...pressupost, seo: e.target.checked}) } />Una consultoría SEO (300€)</label></p>
+        <p><label><input checked={pressupost.ads} type="checkbox" onChange={(e) => setPressupost({...pressupost, ads: e.target.checked}) } />Una campanya de Google Ads (200€)</label></p>
       </Pressupost>
       <h2>Total: {total}€</h2>
     </div>
