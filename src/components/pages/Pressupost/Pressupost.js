399871345f7846ff0ac1fbcfa9cd2b01160448a6
Rubèn Nieto <ruben.np@me.com>
2021-07-18 22:57:45 +0200

    Guarda un pressopost però error (veure comentari Pressupost.js)

--- src/components/pages/Pressupost/Pressupost.js
+++ src/components/pages/Pressupost/Pressupost.js
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react";
 
 // styled components
-import { Formulari } from './Pressupost.styled';
+import { Formulari, Btn } from './Pressupost.styled';
 
 // components
 import Panell from './Panell';
 
 const Pressupost = () => {
-    const [pressupost, setPressupost] = useState(
-      { 
+    const [userLoggedIn, setUserLoggedIn] = useState('guest');
+    const [pressupostActiu, setPressupostActiu] = useState(
+      {
+        id: (Date.parse(new Date())+Math.random()).toString().replace('.', ''),
+        data: new Date().toLocaleString(),
+        user: userLoggedIn,
+        nom: "",
+        client: "",
         web: false, 
         nPags: 0, 
         nIdiomes: 0, 
@@ -16,49 +22,112 @@ const Pressupost = () => {
         ads: false
       });
     const [total, setTotal] = useState(0);
+    const [pressupostos, setPressupostos] = useState([]);
   
-    const chPags = (pags) => setPressupost({...pressupost, nPags: pags});
-    const chIdiomes = (idiomes) => setPressupost({...pressupost, nIdiomes: idiomes});
+    const chPags = (pags) => setPressupostActiu({...pressupostActiu, nPags: pags});
+    const chIdiomes = (idiomes) => setPressupostActiu({...pressupostActiu, nIdiomes: idiomes});
   
-    // agafa dades del pressupost de localStorage, si existeixen, només en inicialitzar
-    useEffect(() => {
-      const onStorage = JSON.parse(localStorage.getItem('pressupost'));
-  
-      if (!onStorage) {
-        localStorage.setItem('pressupost', JSON.stringify(pressupost));
-      } else {
-        setPressupost(onStorage);
-      }
+    // agafa dades del pressupostActiu de localStorage, si existeix, només en inicialitzar
+    useEffect(function init() {
+      setUserLoggedIn('guest');
+      const actiuOnStorage = JSON.parse(localStorage.getItem('pressupostActiu'));
+      if (!actiuOnStorage) localStorage.setItem('pressupostActiu', JSON.stringify(pressupostActiu));
+      else setPressupostActiu(actiuOnStorage);
+
+      const pressupostosOnStorage = JSON.parse(localStorage.getItem('pressupostos'));
+      if (!pressupostosOnStorage) localStorage.setItem('pressupostos', JSON.stringify(pressupostos));
+      else setPressupostos(pressupostosOnStorage);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
   
-    useEffect(() => {
-      // calcula pressupost: 
+    useEffect(function calculaTotal() {
+      // calcula pressupostActiu: 
       let total = 0;
   
-      if (pressupost.web) {
+      if (pressupostActiu.web) {
         total += 500;
-        if (pressupost.nPags > 1 || pressupost.nIdiomes > 1) {
-          total += pressupost.nPags * pressupost.nIdiomes * 30;
+        if (pressupostActiu.nPags > 1 || pressupostActiu.nIdiomes > 1) {
+          total += pressupostActiu.nPags * pressupostActiu.nIdiomes * 30;
         }
       }
-      if (pressupost.seo) total += 300;
-      if (pressupost.ads) total += 200;
+      if (pressupostActiu.seo) total += 300;
+      if (pressupostActiu.ads) total += 200;
   
       setTotal(total);
-  
-      // guarda pressupost a localStorage
-      localStorage.setItem('pressupost', JSON.stringify(pressupost));
-    },[pressupost]);
+      // guarda pressupostActiu a localStorage
+      localStorage.setItem('pressupostActiu', JSON.stringify(pressupostActiu));
+    },[pressupostActiu]);
+
+    useEffect(function savePressupostosOnLocaleStorage() {
+      localStorage.setItem('pressupostos', JSON.stringify(pressupostos));
+    }, [pressupostos]);
+
+    const guardaPressupost = () => {
+      setPressupostos(prev => prev.splice(prev.findIndex(el => el.id === pressupostActiu.id), 1, pressupostActiu));
+    };
+    
+    // TODO: ara mateix si només es canvien els inputs de text, s'ha de clicar 2 vegades "Guardar"
+    //       pq s'actualitzi pressupostos. Si es canvia qualsevol altra opció (a més a més o no dels
+    //       textos) amb només un clic n'hi ha prou ¿¿¿???
   
     return (
       <main>
         <h1>Que vols fer?</h1>
         <Formulari>
-          <p><label><input checked={pressupost.web} type="checkbox" onChange={(e) => setPressupost({...pressupost, web: e.target.checked, nPags: 1, nIdiomes: 1}) } />Una pàgina web (500€)</label></p>
-          { pressupost.web && <Panell p={pressupost} chPags={chPags} chIdiomes={chIdiomes}/> }
-          <p><label><input checked={pressupost.seo} type="checkbox" onChange={(e) => setPressupost({...pressupost, seo: e.target.checked}) } />Una consultoría SEO (300€)</label></p>
-          <p><label><input checked={pressupost.ads} type="checkbox" onChange={(e) => setPressupost({...pressupost, ads: e.target.checked}) } />Una campanya de Google Ads (200€)</label></p>
+          <p>
+            <label>Nom del pressupost: 
+              <input 
+                id="nom"
+                value={pressupostActiu.nom} 
+                type="text" 
+                onChange={e => setPressupostActiu({...pressupostActiu, nom: e.target.value})} 
+              />
+            </label>
+          </p>
+          <p>
+            <label>Client: 
+              <input
+                id="client" 
+                value={pressupostActiu.client} 
+                type="text" 
+                onChange={e => setPressupostActiu({...pressupostActiu, client: e.target.value})} 
+              />
+            </label>
+          </p>
+          <hr />
+          <p>
+            <label>
+              <input 
+                checked={pressupostActiu.web} 
+                type="checkbox" 
+                onChange={e => {
+                  const num = pressupostActiu.web ? 0 : 1;
+                  setPressupostActiu({...pressupostActiu, web: e.target.checked, nPags: num, nIdiomes: num})
+                }} 
+              />Una pàgina web (500€)
+            </label>
+          </p>
+          { pressupostActiu.web && <Panell p={pressupostActiu} chPags={chPags} chIdiomes={chIdiomes}/> }
+          <p>
+            <label>
+              <input 
+                checked={pressupostActiu.seo} 
+                type="checkbox" 
+                onChange={e => setPressupostActiu({...pressupostActiu, seo: e.target.checked})} 
+              />Una consultoría SEO (300€)
+            </label>
+          </p>
+          <p>
+            <label>
+              <input 
+                checked={pressupostActiu.ads} 
+                type="checkbox" 
+                onChange={e => setPressupostActiu({...pressupostActiu, ads: e.target.checked})}
+              />Una campanya de Google Ads (200€)
+            </label>
+          </p>
+          <Btn type="button" onClick={guardaPressupost}>Guarda</Btn>
+          {/* <button type="button">Nou</button> */}
         </Formulari>
         <h2>Total: {total}€</h2>
       </main>
--- src/components/pages/Pressupost/Pressupost.styled.js
+++ src/components/pages/Pressupost/Pressupost.styled.js
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
 
 export const Formulari = styled.form`
+    display: flex;
+    flex-direction: column;
     padding: 20px;
+
+    & > * { margin: 5px; }
+
+    input[type="text"] {
+        padding: 5px;
+        width: 98%;
+        background-color: rgba(0, 0, 0, 0.1);
+        color: rgba(0, 0, 0, 0.75);
+        border: none;
+        font-size: 0.9em;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        &:focus {
+            outline: none;
+            box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2); 
+        }
+    }
+
+    hr {
+        width: 100%;
+        margin: 20px 0;
+        border-width: .1em;
+        border-color: rgba(0, 0, 0, .1);
+        border-style: solid;
+        box-shadow: 1px 1px 5px rgba(0, 0, 0, .1);    
+    }
+`;
+
+export const Btn = styled.button`
+    margin-top: 20px;
+    padding: 5px 10px;
+    background-color: #FFC078;
+    color: #F76707;
+    font-size: 1.2em;
+    font-weight: bold;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    
+    &:hover {
+        background-color: #F76707;
+        color: white;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+    }
 `;
